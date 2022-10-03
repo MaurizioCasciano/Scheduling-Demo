7e11065349d9cd99e12ccffe1a344fe65966df66
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-10-03 17:34:04 +0200

    Spring-Boot Maven Plugin Active Profiles
    https://docs.spring.io/spring-boot/docs/current/maven-plugin/reference/htmlsingle/#run.examples.specify-active-profiles

--- src/main/java/com/example/scheduling/configuration/SchedulingConfiguration.java
+++ src/main/java/com/example/scheduling/configuration/SchedulingConfiguration.java
@@ -1,21 +1,18 @@
 package com.example.scheduling.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
+@Profile("!test")
 @EnableScheduling
-@ConditionalOnProperty(prefix = "scheduling", value = "enabled", havingValue = "true", matchIfMissing = false)
 public class SchedulingConfiguration {
-    @Value("${scheduling.enabled:false}")
-    private Boolean schedulingEnabled;
 
     @PostConstruct
     public void init(){
-        System.out.println("Scheduling enabled: " + this.schedulingEnabled);
+        System.out.println("Scheduling enabled");
     }
 }
