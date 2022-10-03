a7f670bc36a227be1dde6b95680c9fe8e34eb19c
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-10-03 16:27:47 +0200

    Initial commit

---
+++ src/main/java/com/example/scheduling/SchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulingtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduling/SchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulingtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
