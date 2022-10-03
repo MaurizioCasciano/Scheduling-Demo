45929fa2ff3a74eeea659cc8bb871413c3b1bf18
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-10-03 17:03:09 +0200

    BooksController & SpringDoc

---
+++ src/main/java/com/example/scheduling/BooksController.java
@@ -0,0 +1,20 @@
+package com.example.scheduling;
+
+import com.example.scheduling.domain.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("books")
+public class BooksController {
+
+    @GetMapping
+    public Mono<Book> getBook(){
+        return Mono.just(Book.builder()
+                .title("My favourite book")
+                .author("author.name@library.org")
+                .build());
+    }
+}
